7a00374a6143acac849e09fb2f8d7a4f569c62d9
Aaron Gill-Braun <aarongillbraun@gmail.com>
2020-09-07 11:11:08 -0400

    Added full hash_table implementation

--- lib/hash_table.c
+++ lib/hash_table.c
@@ -2,9 +2,239 @@
 // Created by Aaron Gill-Braun on 2020-09-02.
 //
 
-#include <stdlib.h>
 #include "hash_table.h"
+#include <kernel/mem/heap.h>
+#include <stdio.h>
 
 #define _malloc(s) \
   kmalloc(s);
 
+#define _free(ptr) \
+  kfree(ptr);
+
+#define for_in(item, map) \
+  map_entry_t *item;      \
+  for (map_iter_t iter = { map, 0, 0, NULL, NULL, false }; (item = map_next_(&iter, false));)
+
+//
+
+static unsigned default_hasher(char *str) {
+  unsigned hash = 5381;
+  while (*str) {
+    hash = ((hash << 5) + hash) ^ *str++;
+  }
+  return hash;
+}
+
+//
+
+void map_init_(map_base_t *map) {
+  memset(map, 0, sizeof(map_base_t));
+  !map->hasher && (map->hasher = default_hasher);
+  !map->capacity && (map->capacity = INITIAL_MAP_SIZE);
+  map->items = _malloc(map->capacity * sizeof(map_entry_t *));
+  map->size = 0;
+}
+
+void map_free_(map_base_t *map) {
+  for (size_t i = 0; i < map->capacity; i++) {
+    map_entry_t *entry = map->items[i];
+
+    map_entry_t *next;
+    while (entry) {
+      next = entry->next;
+      _free(entry->key);
+      _free(entry->value);
+      _free(entry);
+      entry = next;
+    }
+  }
+
+  _free(map->items);
+}
+
+//
+
+void map_resize_(map_base_t *map, size_t new_size) {
+  map_entry_t **new_items = _malloc(new_size * sizeof(map_entry_t *));
+
+  for_in(entry, map) {
+    unsigned hash = map->hasher(entry->key);
+    unsigned index = hash % new_size;
+
+    if (new_items[index]) {
+      map_entry_t *last = new_items[index];
+      while (last->next) {
+        last = last->next;
+      }
+
+      last->next = entry;
+    } else {
+      new_items[index] = entry;
+    }
+  }
+
+  _free(map->items);
+  map->items = new_items;
+  map->capacity = new_size;
+}
+
+void *map_get_(map_base_t *map, char *key) {
+  unsigned hash = map->hasher(key);
+  unsigned index = hash % map->capacity;
+
+  map_entry_t *item = map->items[index];
+  if (!item) {
+    return NULL;
+  }
+
+  while (item) {
+    unsigned item_hash = map->hasher(item->key);
+    if (item_hash == hash) {
+      return item->value;
+    }
+    item = item->next;
+  }
+
+  return NULL;
+}
+
+void map_set_(map_base_t *map, char *key, void *value, size_t size) {
+  double lf = (double) (map->size + 1) / map->capacity;
+  if (lf > LOAD_FACTOR) {
+    map_resize_(map, map->capacity * 2);
+  }
+
+  unsigned hash = map->hasher(key);
+  unsigned index = hash % map->capacity;
+
+  size_t key_size = strlen(key);
+
+  map_entry_t *entry = _malloc(sizeof(map_entry_t));
+
+  char *tmp_key = _malloc(key_size);
+  void *tmp_value = _malloc(size);
+  memcpy(tmp_key, key, key_size);
+  memcpy(tmp_value, value, size);
+
+  entry->key = tmp_key;
+  entry->value = tmp_value;
+
+  if (map->items[index]) {
+    map_entry_t *curr = map->items[index];
+    while (curr) {
+      unsigned curr_hash = map->hasher(curr->key);
+      if (curr_hash == hash) {
+        _free(curr->key);
+        _free(curr->value);
+        _free(entry);
+        curr->value = tmp_key;
+        curr->value = tmp_value;
+        return;
+      }
+
+      if (!curr->next) break;
+      curr = curr->next;
+    }
+
+    curr->next = entry;
+    return;
+  } else {
+    map->items[index] = entry;
+  }
+
+  map->size++;
+}
+
+void map_delete_(map_base_t *map, char *key) {
+  unsigned hash = map->hasher(key);
+  unsigned index = hash % map->capacity;
+
+  if (map->items[index]) {
+    map_entry_t *entry = map->items[index];
+    unsigned entry_hash = map->hasher(entry->key);
+    if (entry_hash == hash) {
+      map->items[index] = entry->next;
+      _free(entry->key);
+      _free(entry->value);
+      _free(entry);
+      map->size--;
+      return;
+    }
+
+    map_entry_t *last;
+    map_entry_t *curr = entry;
+    while (curr) {
+      unsigned curr_hash = map->hasher(curr->key);
+      if (curr_hash == hash) {
+        last->next = curr->next;
+        _free(curr->key);
+        _free(curr->value);
+        _free(curr);
+        map->size--;
+        return;
+      }
+
+      last = curr;
+      curr = curr->next;
+    }
+  }
+}
+
+//
+
+map_entry_t *map_next_(map_iter_t *iter, bool iter_result) {
+  if (iter->result) {
+    _free(iter->result);
+    iter->result = NULL;
+  }
+
+  if (iter->done || iter->visited >= iter->map->size) {
+    iter->done = true;
+    iter->result = NULL;
+    return NULL;
+  }
+
+  if (iter->last_entry) {
+    if (iter->last_entry->next) {
+      map_entry_t *next = iter->last_entry->next;
+      iter->visited++;
+      iter->last_entry = next;
+
+      if (iter_result) {
+        iter_result_base_t *result = _malloc(sizeof(iter_result_base_t));
+        result->key = next->key;
+        result->value = next->value;
+
+        iter->result = result;
+      }
+
+      return next;
+    }
+
+    iter->last_index++;
+  }
+
+  for (size_t i = iter->last_index; i < iter->map->capacity; i++) {
+    map_entry_t *entry = iter->map->items[i];
+    if (entry) {
+      iter->visited++;
+      iter->last_entry = entry;
+
+      if (iter_result) {
+        iter_result_base_t *result = _malloc(sizeof(iter_result_base_t));
+        result->key = entry->key;
+        result->value = entry->value;
+
+        iter->result = result;
+      }
+      return entry;
+    }
+
+    iter->last_index++;
+  }
+
+  iter->done = true;
+  iter->result = NULL;
+  return NULL;
+}
--- lib/hash_table.h
+++ lib/hash_table.h
@@ -5,31 +5,93 @@
 #ifndef LIB_HASH_TABLE_H
 #define LIB_HASH_TABLE_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 
-typedef unsigned int (*hasher_t)(uint8_t bytes[], size_t len);
+#ifndef INITIAL_MAP_SIZE
+#define INITIAL_MAP_SIZE 128
+#endif
+
+#ifndef LOAD_FACTOR
+#define LOAD_FACTOR 0.75
+#endif
+
+// Macros
+
+#define for_in_map(item, map) \
+  map_entry_t *item;      \
+  for (map_iter_t iter = { map, 0, 0, NULL, NULL, false }; (item = map_next_(&iter));)
+
+// Types
+
+typedef unsigned int (*hasher_t)(char *str);
 
 typedef struct map_entry {
-  unsigned int hash;
+  char *key;
   void *value;
   struct map_entry *next;
 } map_entry_t;
 
 typedef struct {
-  hasher_t hasher;
-  size_t size;
-  size_t capacity;
-  map_entry_t **items;
+  hasher_t hasher;     // hash function
+  size_t size;         // total number of items
+  size_t capacity;     // number of buckets
+  map_entry_t **items; // map entries
 } map_base_t;
 
+typedef struct {
+  char *key;
+  void *value;
+} iter_result_base_t;
+
+typedef struct {
+  map_base_t *map;
+  size_t last_index;
+  size_t visited;
+  map_entry_t *last_entry;
+  iter_result_base_t *result;
+  bool done;
+} map_iter_t;
+
+// Public API
+
 #define map_t(T) \
-  struct { map_base_t base; T *ref; T tmp; } // NOLINT(bugprone-macro-parentheses)
+  struct { map_base_t map; T *ref; T tmp; }
+
+#define iter_result_t(T) \
+  struct { char *key; T *value; }
 
 #define map_init(m) \
-  memset(m, 0, sizeof(*(m)))
+  map_init_(&(m)->map)
 
+#define map_free(m) \
+  map_free_(&(m)->map)
+
+#define map_get(m, key) \
+  ((m)->ref = map_get_(&(m)->map, key))
+
+#define map_set(m, key, value) \
+  ((m)->tmp = (value), map_set_(&(m)->map, key, &(m)->tmp, sizeof((m)->tmp)))
+
+#define map_delete(m, key) \
+  map_delete_(&(m)->map, key)
+
+#define map_iter(m) \
+  { &(m)->map, 0, 0, NULL, NULL, false }
+
+#define map_iter_reset(iter) \
+  (iter)->last_index = 0;    \
+  (iter)->visited = 0;       \
+  (iter)->last_entry = NULL; \
+  (iter)->result = NULL;     \
+  (iter)->done = false;
+
+#define map_next(iter) \
+  (map_next_(iter, true) && (iter)->result ? (void *)(iter)->result : NULL)
+
+// Predefined Map Types
 
 typedef map_t(void*) map_void_t;
 typedef map_t(char*) map_str_t;
@@ -37,5 +99,21 @@ typedef map_t(int) map_int_t;
 typedef map_t(char) map_char_t;
 typedef map_t(float) map_float_t;
 typedef map_t(double) map_double_t;
+
+typedef iter_result_t(void*) iter_result_void_t;
+typedef iter_result_t(char*) iter_result_str_t;
+typedef iter_result_t(int) iter_result_int_t;
+typedef iter_result_t(char) iter_result_char_t;
+typedef iter_result_t(float) iter_result_float_t;
+typedef iter_result_t(double) iter_result_double_t;
+
+// Internal Functions
+
+void map_init_(map_base_t *map);
+void map_free_(map_base_t *map);
+void *map_get_(map_base_t *map, char *key);
+void map_set_(map_base_t *map, char *key, void *value, size_t size);
+void map_delete_(map_base_t *map, char *key);
+map_entry_t *map_next_(map_iter_t *iter, bool iter_result);
 
 #endif
