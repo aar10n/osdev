70da6e3bf43f249eb728622c04dd462e7d5f36d0
Aaron Gill-Braun <aarongillbraun@gmail.com>
2020-09-22 10:08:21 -0400

    Minor file renaming and old file removal

--- include/kernel/cpu/pdt.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Aaron Gill-Braun on 2019-08-05.
-//
-
-#ifndef KERNEL_CPU_PDT_H
-#define KERNEL_CPU_PDT_H
-
-#endif // KERNEL_CPU_PDT_H
--- kernel/cpu/pdt.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Aaron Gill-Braun on 2019-08-05.
-//
-
-#include <kernel/cpu/pdt.h>
