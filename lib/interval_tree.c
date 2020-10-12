7363ccde03218d39b5e43b1abf6fcc085d3cbb3c
Aaron Gill-Braun <aarongillbraun@gmail.com>
2020-10-12 16:01:54 -0400

    Rewrote interval tree to use red-black tree

--- lib/interval_tree.c
+++ lib/interval_tree.c
@@ -16,222 +16,137 @@
 #define _free(ptr) kfree(ptr)
 #endif
 
-static inline bool contains(interval_t i, interval_t j) {
-  return i.start <= j.start && i.end > j.end;
+static inline interval_t intersection(interval_t i, interval_t j) {
+  if (j.start > i.end || i.start > j.end) {
+    return NULL_SET;
+  }
+  return intvl(max(i.start, j.start), min(i.end, j.end));
+}
+
+static inline bool overlaps(interval_t i, interval_t j) {
+  return !is_null_set(intersection(i, j));
 }
 
 static inline bool equals(interval_t i, interval_t j) {
   return i.start == j.start && i.end == j.end;
 }
 
+static inline bool less(interval_t i, interval_t j) {
+  return i.start < j.start;
+}
+
+static inline bool contiguous(interval_t i, interval_t j) {
+  return i.end == j.start;
+}
+
 static inline uint64_t midpoint(interval_t i) {
   return (i.start + i.end) / 2;
 }
 
-static inline interval_t make_union(interval_t i, interval_t j) {
-  interval_t k;
-  k.start = min(i.start, j.start);
-  k.end = max(i.end, j.end);
-  return k;
+static inline interval_t get_interval(rb_node_t *node) {
+  if (node == NULL || node->data == NULL) {
+    return NULL_SET;
+  }
+  return ((intvl_node_t *) node->data)->interval;
 }
 
-static inline uint64_t get_node_max(intvl_node_t *node) {
-  if (node->right) {
-    return get_node_max(node->right);
+static inline uint64_t get_max(rb_node_t *node) {
+  if (node == NULL || node->data == NULL) {
+    return 0;
   }
-  return node->interval.end;
-}
-
-static inline uint64_t get_node_min(intvl_node_t *node) {
-  if (node->left) {
-    return get_node_max(node->right);
-  }
-  return node->interval.start;
-}
-
-static inline bool join_nodes(intvl_node_t *parent, intvl_node_t *a, intvl_node_t *b) {
-  parent->interval = make_union(a->interval, b->interval);
-  parent->is_child = false;
-  parent->data = NULL;
-
-  a->parent = parent;
-  b->parent = parent;
-
-  uint64_t mid = midpoint(parent->interval);
-  if (a->interval.start < mid) {
-    if (b->interval.start < mid || a->interval.end >= b->interval.start) {
-      // interval overlaps existing one
-      return false;
-    }
-    parent->left = a;
-    parent->right = b;
-  } else {
-    if (a->interval.start < mid || b->interval.end >= a->interval.start) {
-      // interval overlaps existing one
-      return false;
-    }
-    parent->left = b;
-    parent->right = a;
-  }
-  return true;
+  return ((intvl_node_t *) node->data)->max;
 }
 
 //
 
-void free_node(intvl_node_t *node) {
-  if (node == NULL) return;
-  if (!node->is_child && node->left != NULL) {
-    free_node(node->left);
-  } else if (!node->is_child && node->right != NULL) {
-    free_node(node->right);
-  }
+void recalculate_max(rb_tree_t *tree, rb_node_t *x) {
+  while (x != tree->nil) {
+    intvl_node_t *xd = x->data;
+    xd->max = max(xd->interval.end, max(get_max(x->left), get_max(x->right)));
 
-  if (node->data) {
-    _free(node->data);
+    x = x->parent;
   }
-  _free(node);
+}
+
+void post_rotate_callback(rb_tree_t *tree, rb_node_t *x, rb_node_t *y) {
+  intvl_node_t *xd = x->data;
+  intvl_node_t *yd = y->data;
+
+  yd->max = xd->max;
+  recalculate_max(tree, x);
+}
+
+void post_insert_callback(rb_tree_t *tree, rb_node_t *z) {
+  recalculate_max(tree, z);
+}
+
+void post_delete_callback(rb_tree_t *tree, rb_node_t *z, rb_node_t *x) {
+  if (z != tree->nil) {
+    _free(((intvl_node_t *) z->data)->data);
+  }
+}
+
+void replace_node_callback(rb_tree_t *tree, rb_node_t *u, rb_node_t *v) {
+  intvl_node_t *ud = u->data;
+  intvl_node_t *vd = v->data;
+  vd->max = ud->max;
 }
 
 //
 
-intvl_node_t *get_node_from_point(intvl_node_t *tree, uint64_t point) {
-  intvl_node_t *node = tree;
-  interval_t interval = (interval_t){ .start = point, .end = point };
-  while (node && contains(node->interval, interval) && !node->is_child) {
-    uint64_t mid = midpoint(node->interval);
-    if (interval.start < mid) {
-      node = node->left;
+rb_node_t *interval_search(rb_tree_t *tree, interval_t i) {
+  rb_node_t *x = tree->root;
+  while (x != tree->nil && !overlaps(i, get_interval(x))) {
+    if (x->left != tree->nil && get_max(x->left) >= i.start) {
+      x = x->left;
     } else {
-      node = node->right;
+      x = x->right;
     }
   }
-  return node;
-}
-
-intvl_node_t *get_node_from_interval(intvl_node_t *tree, interval_t interval) {
-  intvl_node_t *node = tree;
-  while (node && !equals(node->interval, interval)) {
-    uint64_t mid = midpoint(node->interval);
-    if (interval.start < mid) {
-      node = node->left;
-    } else {
-      node = node->right;
-    }
-  }
-  return node;
+  return x;
 }
 
 //
 
-intvl_node_t *create_interval_tree(uint64_t min, uint64_t max) {
-  intvl_node_t *node = _malloc(sizeof(intvl_node_t));
-  node->interval = (interval_t){ min, max };
-  node->is_child = true;
-  node->data = NULL;
-  node->parent = NULL;
-  node->left = NULL;
-  node->right = NULL;
-  return node;
+intvl_tree_t *create_intvl_tree() {
+  rb_tree_t *rb_tree = create_rb_tree();
+  rb_tree->events.post_rotate = post_rotate_callback;
+  rb_tree->events.post_insert_node = post_insert_callback;
+  rb_tree->events.post_delete_node = post_delete_callback;
+  rb_tree->events.replace_node = replace_node_callback;
+
+  intvl_tree_t *tree = _malloc(sizeof(intvl_tree_t));
+  tree->tree = rb_tree;
+  return tree;
 }
 
-bool tree_add_interval(intvl_node_t *tree, interval_t interval, void *data) {
-  _assert(tree != NULL);
-  intvl_node_t *node = tree;
-
-  intvl_node_t *new_node = _malloc(sizeof(intvl_node_t));
-  new_node->interval = interval;
-  new_node->data = data;
-  new_node->is_child = true;
-  new_node->left = NULL;
-  new_node->right = NULL;
-
-  // find the lowest node that fully contains the interval
-  intvl_node_t *parent = NULL;
-  while (node && contains(node->interval, interval)) {
-    uint64_t mid = midpoint(node->interval);
-    parent = node;
-    if (interval.start < mid) {
-      node = node->left;
-    } else {
-      node = node->right;
-    }
-  }
-
-  if (node != NULL) {
-    // create inner node
-    intvl_node_t *inner = _malloc(sizeof(intvl_node_t));
-    bool success = join_nodes(inner, new_node, node);
-    if (success == false) {
-      return false;
-    }
-    new_node = inner;
-  }
-
-  uint64_t mid = midpoint(parent->interval);
-  if (new_node->interval.start < mid) {
-    parent->left = new_node;
-  } else {
-    parent->right = new_node;
-  }
-  return true;
+intvl_node_t *intvl_tree_search(intvl_tree_t *tree, interval_t interval) {
+  rb_node_t *node = interval_search(tree->tree, interval);
+  return node->data;
 }
 
-bool tree_remove_interval(intvl_node_t *tree, interval_t interval) {
-  _assert(tree != NULL);
-
-  intvl_node_t *node = get_node_from_interval(tree, interval);
-  if (node == NULL) {
-    kprintf("interval (%lld, %lld) not found\n", interval.start, interval.end);
-    return false;
-  }
-
-  if (!node->parent) {
-    return false;
-  }
-
-  uint64_t mid = midpoint(node->parent->interval);
-  if (node->interval.start < mid) {
-    node->parent->left = NULL;
-  } else {
-    node->parent->right = NULL;
-  }
-  free_node(node);
-  return true;
+void intvl_tree_insert(intvl_tree_t *tree, interval_t interval, void *data) {
+  intvl_node_t *node_data = _malloc(sizeof(intvl_node_t));
+  node_data->interval = interval;
+  node_data->data = data;
+  rb_tree_insert(tree->tree, interval.start, node_data);
 }
 
-interval_t tree_find_free_interval(intvl_node_t *tree, size_t min_size) {
-  _assert(tree != NULL);
-
-  intvl_node_t *node = tree;
-  if (!node->left->is_child) {
-    interval_t result = tree_find_free_interval(node->left, min_size);
-    if (!IS_ERROR_INTERVAL(result)) {
-      return result;
-    }
-  }
-  if (!node->right->is_child) {
-    interval_t result = tree_find_free_interval(node->right, min_size);
-    if (!IS_ERROR_INTERVAL(result)) {
-      return result;
-    }
-  }
-
-  uint64_t start = get_node_max(node->left);
-  uint64_t end = get_node_min(node->right) - 1;
-
-  if (start != end && end - start >= min_size) {
-    return (interval_t){ start, end };
-  }
-  return ERROR_INTERVAL;
+void intvl_tree_delete(intvl_tree_t *tree, interval_t interval) {
+  rb_tree_delete(tree->tree, interval.start);
 }
 
-intvl_node_t *tree_query_point(intvl_node_t *tree, uint64_t point) {
-  _assert(tree != NULL);
-  return get_node_from_point(tree, point);
+//
+
+intvl_iter_t *intvl_iter_tree(intvl_tree_t *tree) {
+  return rb_iter_tree(tree->tree);
 }
 
-intvl_node_t *tree_query_interval(intvl_node_t *tree, uint64_t start, uint64_t end) {
-  _assert(tree != NULL);
-  interval_t s = {start, end };
-  return get_node_from_interval(tree, s);
+intvl_node_t *intvl_iter_next(intvl_iter_t *iter) {
+  if (!iter->has_next) {
+    return NULL;
+  }
+
+  rb_node_t *node = rb_iter_next(iter);
+  return node->data;
 }
--- lib/interval_tree.h
+++ lib/interval_tree.h
@@ -6,34 +6,39 @@
 #define LIB_INTERVAL_TREE_H
 
 #include <base.h>
+#include <rb_tree.h>
 
-#define IS_ERROR_INTERVAL(i) \
+#define NULL_SET ((interval_t){ UINT64_MAX, 0 })
+
+#define intvl(start, end) \
+  ((interval_t){ start, end })
+
+#define is_null_set(i) \
   ((i).start == UINT64_MAX && (i).end == 0)
-
-#define ERROR_INTERVAL \
-  ((interval_t){ .start = UINT64_MAX, .end = 0 })
-
 
 typedef struct interval {
   uint64_t start;
   uint64_t end;
 } interval_t;
 
-typedef struct intvl_node {
+typedef struct {
   interval_t interval;
-  bool is_child;
+  uint64_t max;
   void *data;
-
-  struct intvl_node *left;
-  struct intvl_node *right;
-  struct intvl_node *parent;
 } intvl_node_t;
 
-intvl_node_t *create_interval_tree(uint64_t min, uint64_t max);
-bool tree_add_interval(intvl_node_t *tree, interval_t interval, void *data);
-bool tree_remove_interval(intvl_node_t *tree, interval_t interval);
-interval_t tree_find_free_interval(intvl_node_t *tree, size_t min_size);
-intvl_node_t *tree_query_point(intvl_node_t *tree, uint64_t point);
-intvl_node_t *tree_query_interval(intvl_node_t *tree, uint64_t start, uint64_t end);
+typedef struct {
+  rb_tree_t *tree;
+} intvl_tree_t;
+
+typedef rb_iter_t intvl_iter_t;
+
+intvl_tree_t *create_intvl_tree();
+intvl_node_t *intvl_tree_search(intvl_tree_t *tree, interval_t interval);
+void intvl_tree_insert(intvl_tree_t *tree, interval_t interval, void *data);
+void intvl_tree_delete(intvl_tree_t *tree, interval_t interval);
+
+intvl_iter_t *intvl_iter_tree(intvl_tree_t *tree);
+intvl_node_t *intvl_iter_next(intvl_iter_t *iter);
 
 #endif
