46e4119ff63ee5775826633446a908bda9f8d7b4
Aaron Gill-Braun <aarongillbraun@gmail.com>
2021-04-03 12:42:31 -0400

    Added usb.c file

---
+++ include/kernel/usb/usb.h
@@ -0,0 +1,8 @@
+//
+// Created by Aaron Gill-Braun on 2021-04-03.
+//
+
+#ifndef KERNEL_USB_USB_H
+#define KERNEL_USB_USB_H
+
+#endif
---
+++ kernel/usb/usb.c
@@ -0,0 +1,5 @@
+//
+// Created by Aaron Gill-Braun on 2021-04-03.
+//
+
+#include <usb/usb.h>
