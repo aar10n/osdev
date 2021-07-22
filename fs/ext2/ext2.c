ea4fd548a9ce404a6c0d3cdfe56a8d5c5e2594a1
Aaron Gill-Braun <aarongillbraun@gmail.com>
2021-07-22 17:30:11 -0400

    start of ext2 driver

---
+++ fs/ext2/ext2.c
@@ -0,0 +1,5 @@
+//
+// Created by Aaron Gill-Braun on 2021-07-22.
+//
+
+#include <ext2/ext2.h>
---
+++ include/fs/ext2/ext2.h
@@ -0,0 +1,10 @@
+//
+// Created by Aaron Gill-Braun on 2021-07-22.
+//
+
+#ifndef FS_EXT2_EXT2_H
+#define FS_EXT2_EXT2_H
+
+#include <fs.h>
+
+#endif
