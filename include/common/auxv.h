a4ade11a332dcf14f63e0b8e345fcb5ce47b6e4c
Aaron Gill-Braun <aarongillbraun@gmail.com>
2021-08-04 22:22:02 -0400

    Added loaded auxilary vectors

---
+++ include/common/auxv.h
@@ -0,0 +1,17 @@
+//
+// Created by Aaron Gill-Braun on 2021-08-03.
+//
+
+#ifndef INCLUDE_COMMON_AUXV_H
+#define INCLUDE_COMMON_AUXV_H
+
+// Auxillary Vectors
+#define AT_PHDR 3
+#define AT_PHENT 4
+#define AT_PHNUM 5
+#define AT_ENTRY 9
+#define AT_EXECPATH 15
+#define AT_RANDOM 25
+#define AT_EXECFN 31
+
+#endif
