aebdb1e831d9079059bd60e9d12b53458174dfe7
Aaron Gill-Braun <aarongillbraun@gmail.com>
2021-08-16 10:02:20 -0400

    Removed old libc files

--- libc/stdio.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Aaron Gill-Braun on 2019-04-21.
-//
-
-#include <stdio.h>
--- libc/stdio.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Aaron Gill-Braun on 2019-04-21.
-//
-
-#ifndef LIBC_STDIO_H
-#define LIBC_STDIO_H
-
-#endif // LIBC_STDIO_H
