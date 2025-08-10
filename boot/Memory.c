bbf5da0e897eb923f90c6ea96109c1b11a7d4b93
Jon Arney <jarneyg@gmail.com>
2025-08-09 20:20:37 -0700

    Issue #4: Bootstrapping musl and gcc together by building a preliminary musl before libgcc and then building the full musl-shared after GCC is finished installing

--- boot/Memory.c
+++ boot/Memory.c
@@ -401,9 +401,11 @@ EFI_STATUS EFIAPI SetupKernelPageTables(IN PAGE_DESCRIPTOR *Descriptors, OUT UIN
     UINT64 PhysAddr = KernelDescriptor->PhysAddr;
     UINTN NumPages = KernelDescriptor->NumPages;
     UINT16 Flags = PageDescriptorFlagsToEntryFlags(KernelDescriptor->Flags) | PE_P;
-
+    // Declaration before label to avoid clang error
+    // error: label followed by a declaration is a C23 extension
+    UINTN N;
   FILL:
-    UINTN N = MIN(NumPages, TABLE_MAX_ENTRIES - PTOffset);
+    N = MIN(NumPages, TABLE_MAX_ENTRIES - PTOffset);
     if (N > 0)
       FillTableWithEntries(UpperPTs[Index], PTOffset, N, PhysAddr, SIZE_4KB, Flags);
     NumPages -= N;
